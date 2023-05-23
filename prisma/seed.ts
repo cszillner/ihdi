f81592fb98e334d1e666de9e19ca51b842606059
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-05-23 10:29:59 -0300

    feat: adding prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client"
+import bcrypt from 'bcryptjs'
+
+const db = new PrismaClient()
+
+const seed = async () => {
+  await db.user.deleteMany()
+
+  await db.user.create({
+    data: {
+      name: 'Usuário Teste',
+      email: 'email@email.com',
+      password: await bcrypt.hash('123456', 10),
+    }
+  })
+
+}
+
+seed().finally(() => db.$disconnect())
